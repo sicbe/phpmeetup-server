061b79c1cb96775a904460f87670c248aa30d421
Bert Lempens <blempens@cvodeverdieping.be>
2017-11-16 11:00:10 +0100

    ready set go

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,11 +10,5 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-
-        // if (!$token = JWTHAuth::attempt($credentials)) {
-        //     return response()->json(['error' => 'invalid_credentials'], 401);
-        // }
-
-        // return response()->json(compact('token', 'user'));
     }
 }
